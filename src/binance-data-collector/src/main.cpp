a000e449023418fed1de54eb162321155f23c2e3
Zhora Gevorgyan <jora.4.gevorgyan@gmail.com>
2026-03-02 21:38:54 +0400

    add Config implementation

--- src/binance-data-collector/src/main.cpp
+++ src/binance-data-collector/src/main.cpp
@@ -1,36 +1,28 @@
-#include "spdlog/sinks/basic_file_sink.h"
-#include "spdlog/spdlog.h"
-
-#include <boost/program_options.hpp>
-
+#include "Config/Config.hpp"
 #include <iostream>
 
 namespace DataCollector {
 
 int mainImpl(int argc, char* argv[]) {
-	// Register logger with hardcoded YET(get it from config in future)
-	try {
-		auto file_sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>(
-		    "/var/log/binance-data-collector/current.log", true);
-		std::vector<spdlog::sink_ptr> sinks{file_sink};
-		auto logger = std::make_shared<spdlog::logger>("binance", sinks.begin(),
-		                                               sinks.end());
-		spdlog::register_logger(logger);
-		spdlog::set_default_logger(logger);
-		spdlog::set_level(spdlog::level::info);
-		spdlog::flush_on(spdlog::level::err);
-
-		spdlog::info("Binance Data Collector started");
-
-		for (int i = 0; i < argc; ++i) {
-			spdlog::info("Argument {}: {}", i, argv[i]);
-		}
-
-	} catch (const spdlog::spdlog_ex& ex) {
-		std::cerr << "Log initialization failed: " << ex.what() << std::endl;
+	auto& config = Config::Config::getInstance();
+	if (config == nullptr) {
+		std::cerr << "Failed to create Config instance" << std::endl;
+		return EXIT_FAILURE;
+	}
+	if (!config->init(argc, argv)) {
+		return EXIT_FAILURE;
+	}
+	if (config->isHelp()) {
+		config->printHelp();
+		return EXIT_SUCCESS;
+	}
+	if (!(config->initLogger() && config->updateConfig())) {
+		return EXIT_FAILURE;
 	}
 
-	return 0;
+	spdlog::info("Binance Data Collector started");
+
+	return EXIT_SUCCESS;
 }
 
 } // namespace DataCollector
