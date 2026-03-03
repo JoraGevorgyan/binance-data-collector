51f8fceca6dc86b16546bc9473ada4a46d6abd12
Zhora Gevorgyan <jora.4.gevorgyan@gmail.com>
2026-03-03 16:35:53 +0400

    spelling mistake is fixed

--- cmake/paths_and_names.cmake
+++ cmake/paths_and_names.cmake
@@ -1,5 +1,5 @@
 set(EXECUTABLE_NAME "binance-data-collector")
-set(APP_NAME "BinanceDatCollectorService")
+set(APP_NAME "BinanceDataCollectorService")
 set(NAME_SERVICE "binance-data-collector-service")
 
 set(LOGGER_NAME "spdlog_header_only")
