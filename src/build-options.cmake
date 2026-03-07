ffb2faaa628519c966e5ac0ed572bcec39b380ad
Zhora Gevorgyan <jora.4.gevorgyan@gmail.com>
2026-03-08 00:43:26 +0400

    combine compile options together

--- src/build-options.cmake
+++ src/build-options.cmake
@@ -1,9 +1,18 @@
 # this needs to be imporved yet, but yet, for now we use clang only:D
 if (CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
     if (CMAKE_BUILD_TYPE STREQUAL "Debug")
-        add_compile_options(-g3)
+        add_compile_options(
+			"-Wall"
+			"-Wextra"
+			"-g3")
     elseif (CMAKE_BUILD_TYPE STREQUAL "Release")
-        add_compile_options(-O3 -flto)
+        add_compile_options(
+			"-Wall"
+			"-Wextra"
+			"-Werror"
+			"-O3"
+			"-flto"
+        )
         add_link_options(-flto)
     endif()
 endif()
