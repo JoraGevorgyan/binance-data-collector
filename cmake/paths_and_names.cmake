12486f02d27ff428a1b60558dd405b2653212263
Zhora Gevorgyan <jora.4.gevorgyan@gmail.com>
2026-03-07 11:13:54 +0400

    update logname

--- cmake/paths_and_names.cmake
+++ cmake/paths_and_names.cmake
@@ -2,4 +2,4 @@ set(EXECUTABLE_NAME "binance-data-collector")
 set(APP_NAME "BinanceDataCollectorService")
 set(NAME_SERVICE "binance-data-collector-service")
 
-set(LOGGER_NAME "spdlog_header_only")
+set(LOGGER_NAME "spdlog::spdlog_header_only")
