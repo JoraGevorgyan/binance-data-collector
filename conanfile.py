47b4fbcacd02a55f0c5a619fddb5d83f08e256ed
Zhora Gevorgyan <jora.4.gevorgyan@gmail.com>
2026-03-07 11:02:06 +0400

    add concnafile

---
+++ conanfile.py
@@ -0,0 +1,21 @@
+from conan import ConanFile
+from conan.tools.cmake import CMakeDeps, CMakeToolchain, cmake_layout
+
+
+class BinanceDataCollectorConan(ConanFile):
+    name = "binance-data-collector"
+    version = "1.0.0"
+    description = "Binance market data collection and aggregation service (WebSocket)."
+
+    def layout(self):
+        cmake_layout(self)
+
+    def requirements(self):
+        self.requires("boost/1.83.0")
+        self.requires("openssl/3.2.1")
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.generate()
+        deps = CMakeDeps(self)
+        deps.generate()
